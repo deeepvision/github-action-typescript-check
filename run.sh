2882044824f2423e9832d2638253bf09952a8970
Sergii Sadovyi <1175636+sergsadovyi@users.noreply.github.com>
2021-11-25 12:11:14 +0200

    feat: add deepvision npm registry auth

--- run.sh
+++ run.sh
@@ -4,6 +4,11 @@ set -e
 
 echo ""
 
+if [[ -z "${GITHUB_TOKEN}" ]]; then
+  echo "@deeepvision:registry=https://npm.pkg.github.com" >> .npmrc
+  echo "//npm.pkg.github.com/:_authToken=${GITHUB_TOKEN}" >> .npmrc
+fi
+
 echo "Install packages"
 npm install
 
